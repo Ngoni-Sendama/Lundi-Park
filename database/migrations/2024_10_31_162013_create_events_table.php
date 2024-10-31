9a5313d5b019978af1307423fd484e1e95a83568
Ngoni-Sendama <ngonidzashesendama@gmail.com>
2024-10-31 22:22:55 +0530

    Migrations

---
+++ database/migrations/2024_10_31_162013_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_category_id');
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('date')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->text('details')->nullable();
+            $table->text('tags')->nullable();
+            $table->text('keywords')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
---
+++ database/migrations/2024_10_31_163630_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('blog_category_id');
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('date')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->text('details')->nullable();
+            $table->text('tags')->nullable();
+            $table->text('keywords')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
